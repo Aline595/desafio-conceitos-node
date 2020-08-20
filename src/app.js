fa817861940b3b52e54ed8fae509163515d7da3c
unknown <aline.as385@gmail.com>
2020-08-20 16:17:26 -0300

    likes certo

--- src/app.js
+++ src/app.js
@@ -61,6 +61,7 @@ app.put("/repositories/:id", (request, response) => {
     title,
     url,
     techs,
+    likes: repositories[repositoryIndex].likes,
   }
 
   repositories[repositoryIndex] = repository;
