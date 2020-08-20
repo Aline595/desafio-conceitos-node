6d255cb0c0e110e6514a832b729f2ce030ee31af
unknown <aline.as385@gmail.com>
2020-08-20 14:31:30 -0300

    funcionalidades

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid, isUuid } = require("uuidv4");
 
 // const { v4: uuid } = require('uuid');
 
@@ -10,24 +11,122 @@ app.use(cors());
 
 const repositories = [];
 
+function validaId(request, response, next){
+  const { id } = request.params;
+
+  if( !isUuid(id) ){
+    return response.status(400).json({ error: "Invalid repository ID." });
+  }
+  return next();
+}
+
+function validaLikes(request, response, next){
+  const { likes } = request.params;
+
+  if ( likes > 0){
+    return response.status(400).json({ error: "Número de likes maior que zero" });
+  }
+
+  return next();
+}
+
+//GET /repositories: Rota que lista todos os repositórios;
 app.get("/repositories", (request, response) => {
-  // TODO
+  
+  return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+/*
+POST /repositories: A rota deve receber title, url e techs dentro do corpo 
+da requisição, sendo a URL o link para o github desse repositório. Ao cadastrar
+um novo projeto, ele deve ser armazenado dentro de um objeto no seguinte
+formato: { id: "uuid", title: 'Desafio Node.js', url: 'http://github.com/...',
+techs: ["Node.js", "..."], likes: 0 }; Certifique-se que o ID seja um UUID, 
+e de sempre iniciar os likes como 0.
+*/
+app.post("/repositories", validaId, validaLikes, (request, response) => {
+  const  { title, url,  techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
+/***
+ * PUT /repositories/:id: A rota deve alterar apenas o title, 
+ * a url e as techs do repositório que possua o id igual 
+ * ao id presente nos parâmetros da rota;
+ */
 app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
+  const { id } = request.params;
+  const { title, url, techs} = request.params;
 
+  const repositoryIndex = repository.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0){
+    return response.status(400).json({ error: "Repository not found." });
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+  }
+
+  repositories[repositoryIndex] = repository;
+
+});
+/***
+ * DELETE /repositories/:id: A rota deve deletar o repositório com o 
+ * id presente nos parâmetros da rota;
+ */
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+  const { id } = request.params;
 
+  const repositoryIndex = repository.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0){
+    return response.status(400).json({ error: "Repository not found." });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+});
+/***
+ * POST /repositories/:id/like: A rota deve aumentar o número de likes do 
+ * repositório específico escolhido através do id presente nos parâmetros da
+ *  rota, a cada chamada dessa rota, o número de likes deve ser aumentado em 1;
+ */
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id, like } = request.params;
+  
+  const aumentaLikes = like++;
+
+  const repositoryIndex = repository.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0){
+    return response.status(400).json({ error: "Repository not found." });
+  }
+
+  const repository = {
+    id, 
+    like,
+  }
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repositories);
+
 });
 
 module.exports = app;
